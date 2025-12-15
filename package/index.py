c540661594b42a67d7b4d76f873ce2b7474e4bb0
Heatherann Jackson <heatherann.m.jackson@gmail.com>
2025-12-14 19:36:49 -0600

    fixed lambda function again

--- package/index.py
+++ package/index.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import sys
 import uuid
+import json
 from urllib.parse import unquote_plus
 from PIL import Image
 import PIL.Image
@@ -15,11 +16,16 @@ def resize_image(image_path, resized_path):
             
 def handler(event, context):
   for record in event['Records']:
-    bucket = record['s3']['bucket']['name']
-    key = unquote_plus(record['s3']['object']['key'])
-    tmpkey = key.replace('/', '')
-    download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
-    upload_path = '/tmp/resized-{}'.format(tmpkey)
-    s3_client.download_file(bucket, key, download_path)
-    resize_image(download_path, upload_path)
-    s3_client.upload_file(upload_path, 'sqs-lab-output-bucket', 'resized-{}'.format(key))
+    sns_message = json.loads(record['body'])
+
+    s3_event = json.loads(sns_message['Message'])
+
+    for s3_record in s3_event['Records']:
+      bucket = s3_record['s3']['bucket']['name']
+      key = unquote_plus(s3_record['s3']['object']['key'])
+      tmpkey = key.replace('/', '')
+      download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
+      upload_path = '/tmp/resized-{}'.format(tmpkey)
+      s3_client.download_file(bucket, key, download_path)
+      resize_image(download_path, upload_path)
+      s3_client.upload_file(upload_path, 'sqs-lab-output-bucket', 'resized-{}'.format(key))
